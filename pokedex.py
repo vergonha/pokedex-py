d96b8243177ccad177d88a6db401e2e1b231d7a1
Yuri <yurigustavogross@gmail.com>
2022-04-16 19:31:45 -0300

    Mudanca na estrutura geral do código

--- pokedex.py
+++ pokedex.py
@@ -1,39 +1,10 @@
 from flask import Flask, redirect, render_template, url_for, request
 import requests
 import json
-import os
+import findpokebyname
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-def findPokeByName(pokeName):
-    try:
-        pokemonName = pokeName.lower()
-        request = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemonName}")
-        todo = json.loads(request.content)
-
-        spriteTodo = todo['sprites']
-        sprite = spriteTodo["front_default"]
-        type_primary = todo['types'][0]['type']['name']
-        try:
-            type_secondary = todo['types'][1]['type']['name']
-        except:
-            type_secondary = "Nenhum"
-        weight = todo['weight']/10
-        height = todo['height']/10
-
-        hp = todo['stats'][0]['base_stat']
-        attack = todo['stats'][1]['base_stat']
-        defense = todo['stats'][2]['base_stat']
-        special_attack = todo['stats'][3]['base_stat']
-        special_defense = todo['stats'][4]['base_stat']
-        speed = todo['stats'][5]['base_stat']
-
-        
-        
-
-        return type_primary, type_secondary, weight, height, sprite, hp, attack, defense, special_attack, special_defense, speed
-    except:
-        return "Pokemon não encontrado!"
 
 
 @app.route("/")
@@ -45,24 +16,31 @@ def index():
 def pokemon():
     pokeName = request.form.get('pokemon')
     pokeName = pokeName.capitalize()
-    infos = findPokeByName(pokeName)
-    type_primary = infos[0].capitalize()
-    type_secondary = infos[1].capitalize()
-    weight = infos[2]
-    height = infos[3]
-    sprite = infos[4]
+    try:
+        pokemon = findpokebyname.findPokeByName(pokeName)
+        nomePokemon = pokemon.nomePokemon
+        type_primary = pokemon.type_primary
+        type_secondary = pokemon.type_secondary
+        weight = pokemon.weight
+        height = pokemon.height
+        sprite = pokemon.sprite
+        hp = pokemon.hp
+        attack = pokemon.attack
+        defense = pokemon.defense
+        special_attack = pokemon.special_attack
+        special_defense = pokemon.special_defense
+        speed = pokemon.speed
+    except:
+        pokemon = None
 
-    hp = infos[5]
-    attack = infos[6]
-    defense = infos[7]
-    special_attack = infos[8]
-    special_defense = infos[9]
-    speed = infos[10]
 
-    if infos != "Pokemon não encontrado!":
+    if pokemon == None:
+        return redirect(url_for('error'))
+    else:
         return render_template('pokemon.html', 
-            type_primary=type_primary, 
-            type_secondary=type_secondary, 
+            nomePokemon=nomePokemon.capitalize(),
+            type_primary=type_primary.capitalize(), 
+            type_secondary=type_secondary.capitalize(), 
             weight=weight, 
             height=height, 
             pokeName=pokeName, 
@@ -74,8 +52,6 @@ def pokemon():
             special_defense=special_defense,
             speed=speed,
             )
-    else:
-        return redirect(url_for('error'))
 
 @app.route("/error")
 def error():
