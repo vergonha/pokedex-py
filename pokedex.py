3fb1f031b8ab4765d4c49047a4d11c280406d177
Yuri <yurigustavogross@gmail.com>
2022-04-16 19:45:35 -0300

    Tratamento de erros

--- pokedex.py
+++ pokedex.py
@@ -16,46 +16,44 @@ def index():
 def pokemon():
     pokeName = request.form.get('pokemon')
     pokeName = pokeName.capitalize()
+    
     try:
         pokemon = findpokebyname.findPokeByName(pokeName)
-        nomePokemon = pokemon.nomePokemon
-        type_primary = pokemon.type_primary
-        type_secondary = pokemon.type_secondary
-        weight = pokemon.weight
-        height = pokemon.height
-        sprite = pokemon.sprite
-        hp = pokemon.hp
-        attack = pokemon.attack
-        defense = pokemon.defense
-        special_attack = pokemon.special_attack
-        special_defense = pokemon.special_defense
-        speed = pokemon.speed
     except:
-        pokemon = None
+        return render_template('error.html')
 
+    nomePokemon = pokemon.nomePokemon
+    type_primary = pokemon.type_primary
+    type_secondary = pokemon.type_secondary
+    weight = pokemon.weight
+    height = pokemon.height
+    sprite = pokemon.sprite
+    hp = pokemon.hp
+    attack = pokemon.attack
+    defense = pokemon.defense
+    special_attack = pokemon.special_attack
+    special_defense = pokemon.special_defense
+    speed = pokemon.speed
 
-    if pokemon == None:
-        return redirect(url_for('error'))
-    else:
-        return render_template('pokemon.html', 
-            nomePokemon=nomePokemon.capitalize(),
-            type_primary=type_primary.capitalize(), 
-            type_secondary=type_secondary.capitalize(), 
-            weight=weight, 
-            height=height, 
-            pokeName=pokeName, 
-            sprite=sprite,
-            hp=hp,
-            attack=attack,
-            defense=defense,
-            special_attack=special_attack,
-            special_defense=special_defense,
-            speed=speed,
-            )
+    return render_template('pokemon.html', 
+        nomePokemon=nomePokemon.capitalize(),
+        type_primary=type_primary.capitalize(), 
+        type_secondary=type_secondary.capitalize(), 
+        weight=weight, 
+        height=height, 
+        pokeName=pokeName, 
+        sprite=sprite,
+        hp=hp,
+        attack=attack,
+        defense=defense,
+        special_attack=special_attack,
+        special_defense=special_defense,
+        speed=speed,
+        )
 
 @app.route("/error")
 def error():
     return render_template('error.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
